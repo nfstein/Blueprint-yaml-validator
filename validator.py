75e4bf967dee422c7290c98b3eb164917dcaa090
Nishu.Bharti1 <Nishu.Bharti1@ibm.com>
2022-11-02 16:21:41 +0530

    addtion validation rules

--- validator.py
+++ validator.py
@@ -1,16 +1,30 @@
+from operator import truediv
 import yamale
 import uuid
 import re
 from yamale.validators import DefaultValidators, Validator, Regex
 
+class TFVERSION(Validator):
+    """ Custom TFVERSION validator """
+    tag = 'settings'
+
+    def __init__(self, *args, **kwargs):
+        self._settings = str(kwargs.get('settings', ''))
+        for k,v in self._settings:
+            if k == "TF_VERSION":
+                return True
+
+        return False
+
 validators = DefaultValidators.copy()  # This is a dictionary
+validators[TFVERSION.tag] = TFVERSION
 schema = yamale.make_schema('./schema.yaml', validators=validators)
 
+#example data sets
 data = yamale.make_data('./detection-rule.yaml')
-# data = yamale.make_data('./test-rule1.yaml')
-# data = yamale.make_data('./test-rule2.yaml')
-# data = yamale.make_data('./test-rule3.yaml')
-
+# data = yamale.make_data('./test-sample/test-rule1.yaml')
+# data = yamale.make_data('./test-sample/test-rule2.yaml')
+# data = yamale.make_data('./test-sample/test-rule3.yaml')
 
 try:
     yamale.validate(schema, data)
